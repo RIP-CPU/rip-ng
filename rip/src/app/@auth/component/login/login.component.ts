df5a9029482a825b6b3e42c33f5055bf6de1f907
Ridla Fadilah <ridla.fadilah@gmail.com>
2019-01-08 20:30:46 +0700

    initiate project

--- rip/src/app/@auth/component/login/login.component.ts
+++ rip/src/app/@auth/component/login/login.component.ts
@@ -12,8 +12,10 @@ declare var $: any;
 })
 export class LoginComponent {
 
-    private buttonLogin:boolean = false;
+    public buttonLogin:boolean = false;
     private progressBar:number = 25;
+    public username:string;
+    public password:string;
 
     form: FormGroup = new FormGroup({
       username: new FormControl(),
@@ -23,46 +25,54 @@ export class LoginComponent {
     constructor(private router: Router, private authTokenService:AuthTokenService){}
 
     login(){
-      $('body').removeClass('pace-done');
-      $('body').addClass('pace-running');
-      $('.pace').removeClass('pace-inactive');
-      $('.pace').addClass('pace-active');
-      if(this.progressBar<35){
-        this.progressBar = 35;
-        $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
-        $('.pace-progress').attr('data-progress', this.progressBar);
-        $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
-      }
-      this.buttonLogin = true;
-      this.authTokenService.login(
-        this.form.get("username").value,
-        this.form.get("password").value)
-        .then(()=>{
-            this.progressBar = 90;
-            $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
-            $('.pace-progress').attr('data-progress', this.progressBar);
-            $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
-            this.progressBar = 0;
-            this.router.navigate(["/app/dashboard"]);
-        })
-        .catch(error=>{
-            this.progressBar = 85;
-            $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
-            $('.pace-progress').attr('data-progress', this.progressBar);
-            $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
-            $('body').removeClass('pace-running');
-            $('body').addClass('pace-done');
-            $('.pace').removeClass('pace-active');
-            $('.pace').addClass('pace-inactive');
-            this.progressBar = 0;
-            this.buttonLogin = false;
-            console.log(error);
-        });
-        if(this.progressBar >= 35 && this.progressBar<65){
-            this.progressBar = 65;
-            $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
-            $('.pace-progress').attr('data-progress', this.progressBar);
-            $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
+      this.username = this.form.get("username").value;
+      this.password = this.form.get("password").value;
+      if(this.username && this.password){
+        $('body').removeClass('pace-done');
+        $('body').addClass('pace-running');
+        $('.pace').removeClass('pace-inactive');
+        $('.pace').addClass('pace-active');
+        if(this.progressBar<35){
+          this.progressBar = 35;
+          $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
+          $('.pace-progress').attr('data-progress', this.progressBar);
+          $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
         }
+        this.buttonLogin = true;
+        this.authTokenService.login(
+          this.form.get("username").value,
+          this.form.get("password").value)
+          .then(()=>{
+              this.progressBar = 90;
+              $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
+              $('.pace-progress').attr('data-progress', this.progressBar);
+              $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
+              this.progressBar = 0;
+              this.username = null;
+              this.password = null;
+              this.router.navigate(["/app/dashboard"]);
+          })
+          .catch(error=>{
+              this.username = null;
+              this.password = null;
+              this.progressBar = 85;
+              $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
+              $('.pace-progress').attr('data-progress', this.progressBar);
+              $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
+              $('body').removeClass('pace-running');
+              $('body').addClass('pace-done');
+              $('.pace').removeClass('pace-active');
+              $('.pace').addClass('pace-inactive');
+              this.progressBar = 0;
+              this.buttonLogin = false;
+              console.log(error);
+          });
+          if(this.progressBar >= 35 && this.progressBar<65){
+              this.progressBar = 65;
+              $('.pace-progress').attr('data-progress-text', this.progressBar+'%');
+              $('.pace-progress').attr('data-progress', this.progressBar);
+              $('.pace-progress').attr('style', 'transform: translate3d('+this.progressBar+'%, 0px, 0px);');
+          }
+      }
     }
 }
